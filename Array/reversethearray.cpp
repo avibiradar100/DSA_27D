f47e2233b1cbfbbb9fc5e37f6aecc752f89c4749
Avinash Biradar <biradarav.100@gmail.com>
2023-01-15 15:54:59 +0530

    Reverse the array

---
+++ Array/reversethearray.cpp
@@ -0,0 +1,29 @@
+// time complexity --> O(n)
+// space --> O(1)
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+//     int n;
+//     cin>>n;
+//     int arr[n];
+//     for(int i=0;i<n;i++)
+//         cin>>arr[i];
+//     for(int i=0,j=n-1;i<j;i++,j--)
+//     {
+//         int tmp=arr[i];
+//         arr[i]=arr[j];
+//         arr[j]=tmp;
+        
+//         // or swap(arr[i],arr[j])
+//     }
+//     cout<<"\nAfter Sorting: ";
+//     for(int i=0;i<n;i++)
+//         cout<<arr[i]<<" ";
+//     return 0;
+// }
+
+
+
