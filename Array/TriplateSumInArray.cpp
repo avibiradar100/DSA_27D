8859391b7d138fb6e29492670f6bcc0acc749353
Avinash Biradar <biradarav.100@gmail.com>
2023-01-15 12:52:21 +0530

    Triple Sum in array

---
+++ Array/TriplateSumInArray.cpp
@@ -0,0 +1,10 @@
+// Given an array arr of size n and an integer X. Find if there's a triplet in the array which sums up to the given integer X.
+
+// App1:
+// Time: O(n^3)
+// Space: O(1)
+
+// App2: O(n^2)   using unorderd set 
+// space:O(n)
+
+
