e97c565bb84dc96fa8c509050361a623648d6ed8
Avinash Biradar <biradarav.100@gmail.com>
2023-01-15 16:02:04 +0530

    max and min element in the array

---
+++ Array/MaxAndMin.cpp
@@ -0,0 +1,32 @@
+
+// App1:
+/*
+    time complexity--> O(n)
+    space Complexity-->O(1)
+*/
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+//     int n;
+//     cin>>n;
+//     int arr[n];
+//     for(int i=0;i<n;i++)
+//         cin>>arr[i];
+//     int max=0,min=INT_MAX;
+//     for(int i=0;i<n;i++)
+//     {
+//         if(arr[i]>max)
+//             max=arr[i];
+//         if(arr[i]<min)
+//             min=arr[i];
+//     }
+//     cout<<"Max Element:"<<max<<"\nMin Element:"<<min;
+//     return 0;
+// }
+
+
+//App2:
+
+
