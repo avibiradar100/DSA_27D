f9069ff9e9241d191cbcdad3ad5c0fb0a4ed10ed
Avinash Biradar <biradarav.100@gmail.com>
2023-01-20 17:44:04 +0530

    Detect Loop in Linked List

--- LinkedList/DetectLoop.cpp
+++ LinkedList/DetectLoop.cpp
@@ -2,3 +2,58 @@
 
 // Time:O(N)
 // Space:O(N)
+
+// bool detectLoop(Node* head)
+//     {
+//         // your code here
+//         unordered_set<Node*> uset;
+        
+//         Node *tmp=head;
+//         while(tmp){
+            
+//             if(uset.find(tmp)!=uset.end())
+//               return true;
+//             uset.insert(tmp);
+//             tmp=tmp->next;
+//         }
+//         return false;
+//     }
+
+//APP2: Modifed data like out of range values and if visit again means loop present
+
+// Time:O(N)
+// Space:O(N)
+
+//  bool detectLoop(Node* head)
+//     {
+//         // your code here
+//         Node *tmp=head;
+//         while(tmp){
+            
+//             if(tmp->data==-1)
+//               return true;
+//             tmp->data=-1;
+//             tmp=tmp->next;
+//         }
+//         return false;
+//     }
+
+//App3:  Fast and slow pointer method
+
+//Time:O(N)
+// Space:O(N)
+
+// bool detectLoop(Node* head)
+//     {
+//         // your code here
+//         Node *slow=head,*fast=head;
+//         while(fast && fast->next){
+            
+//             slow=slow->next;
+//             fast=fast->next->next;
+            
+//             if(slow==fast)
+//               return true;
+//         }
+//         return false;
+//     }
