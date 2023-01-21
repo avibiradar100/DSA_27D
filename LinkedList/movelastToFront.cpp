0128106b99eb509891c82fbf3c3f9b05ededa1eb
Avinash Biradar <biradarav.100@gmail.com>
2023-01-21 15:59:41 +0530

    move last element to front of linked list

---
+++ LinkedList/movelastToFront.cpp
@@ -0,0 +1,21 @@
+
+Time:O(N)
+Space:O(1)
+
+// void moveToFront(Node* head)
+// {
+//     if (!head || !head->next)
+//         return;
+    
+//     Node *last=head,*secLast;
+
+//     while (last->next) {
+//         secLast = last;
+//         last = last->next;
+//     }
+ 
+//     secLast->next = NULL;
+//     last->next = head;
+    
+//     head= last;
+// }
